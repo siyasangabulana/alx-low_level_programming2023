fc0bc483766e8cd5c5a60f4b6ab3c2d3f9680540
siyasangabulana <bulanasiyasanga@gmail.com>
2023-06-15 06:02:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that prints a linewith puts function 
+ * main - A program that prints a line with puts function 
  *
  * Return: Always 0 (Success)
  */
