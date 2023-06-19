a8a41e92b27bc23c4197ca1c7ccfa2a08948df41
siyasangabulana <bulanasiyasanga@gmail.com>
2023-06-19 07:00:42 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number;
+
+for (number = 0; number <= 9; number++)
+putchar(number + 48);
+
+putchar('\n');
+
+return (0);
+}
