fa9b5614d39a5ea1b2cd1888184fcfc5053febae
siyasangabulana <bulanasiyasanga@gmail.com>
2023-06-19 06:10:52 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-    char letter = 'a';
+char letter = 'a';
 
-    while (letter <= 'z')
-    {
-        putchar(letter);
-        letter++;
-    }
-    putchar('\n');
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
 
-    return (0);
+return (0);
 }
