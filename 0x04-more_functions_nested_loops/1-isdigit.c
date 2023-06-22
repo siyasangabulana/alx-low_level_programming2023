0d75df0d690a80e0bddaa71198bd0c9d37b37da5
siyasangabulana <bulanasiyasanga@gmail.com>
2023-06-22 06:21:07 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int _isdigit(int c);
+
+int main(void)
+{
+    char c;
+
+    c = '0';
+    printf("%c: %d\n", c, _isdigit(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isdigit(c));
+    return 0;
+}
