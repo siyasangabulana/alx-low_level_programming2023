567214b507227dca1148f82593ea2c9eec118e9c
siyasangabulana <bulanasiyasanga@gmail.com>
2023-06-22 07:56:19 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *It is function that draws a straight line in the terminal.
+ *Return: 0 Always 
+ */
+int main(void)
+{
+    print_line(0);
+    print_line(2);
+    print_line(10);
+    print_line(-4);
+    return (0);
+}
+
