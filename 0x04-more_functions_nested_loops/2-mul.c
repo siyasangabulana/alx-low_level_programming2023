f96ca0e452625ab001a6e2a5f62ac047368fa92b
siyasangabulana <bulanasiyasanga@gmail.com>
2023-06-22 06:58:12 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ */
+
+int mul(int a, int b);
+
+int main(void)
+{
+    printf("%d\n", mul(98, 1024));
+    printf("%d\n", mul(-402, 4096));
+    return (0);
+}
+
