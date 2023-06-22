57727b1a23fe0b4b678114fdd94fdb1b4fe19429
siyasangabulana <bulanasiyasanga@gmail.com>
2023-06-22 10:01:13 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,6 +4,7 @@
  *It function that prints 10 times the numbers, from 0 to 14, followed by a new line.
  *Return: 10 times of the number up to 0 to 14
  */
+
 void more_numbers(void)
 {
 int i, j;
@@ -21,4 +22,3 @@ _putchar((j % 10) + '0');
 _putchar('\n');
 }
 }
-
