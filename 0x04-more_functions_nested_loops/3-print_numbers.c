1a2014718646bed26240dc687b527a82b485a9c7
siyasangabulana <bulanasiyasanga@gmail.com>
2023-06-22 09:49:25 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,7 +4,8 @@
  * print_numbers - Prints the numbers from 0 to 9, followed by a new line.
  * Loop from 0 to 9 
  * Convert the digit to a character and print it using _putchar
- * Print a new line character using _putchar 
+ * Print a new line character using _putchar
+ * Return: The number since 0 up to 9 
 */
 
 void print_numbers(void)
