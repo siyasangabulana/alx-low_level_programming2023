be6af35602818ddbdf878a93bc465a93ad28a703
siyasangabulana <bulanasiyasanga@gmail.com>
2023-06-26 05:59:32 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,10 +14,11 @@ void print_rev(char *s)
 int length = 0;
 while (s[length] != '\0')
 length++;
-while (length >= 0)
+
+while (length > 0)
 {
-_putchar(s[length]);
 length--;
+_putchar(s[length]);
 }
 _putchar('\n');
 }
