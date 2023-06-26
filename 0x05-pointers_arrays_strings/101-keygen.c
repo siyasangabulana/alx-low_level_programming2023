4afe8a772bb33287345fbd83f7bedd6b5d393794
siyasangabulana <bulanasiyasanga@gmail.com>
2023-06-26 07:37:25 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define PASSWORD_LENGTH 10
+int main(void)
+{
+char password[PASSWORD_LENGTH + 1];
+const char valid_chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+int i;
+srand(time(NULL));
+for (i = 0; i < PASSWORD_LENGTH; i++)
+{
+int index = rand() % (sizeof(valid_chars) - 1);
+password[i] = valid_chars[index];
+}
+password[PASSWORD_LENGTH] = '\0';
+printf("%s\n", password);
+return (0);
+}
