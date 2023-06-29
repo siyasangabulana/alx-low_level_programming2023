d43f95f130a9c49aa64b5aaa19e85db02fa0d958
siyasangabulana <bulanasiyasanga@gmail.com>
2023-06-29 05:20:10 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,37 +1,11 @@
-#include "main.h"
 #include <stdio.h>
 
-int _putchar(char c);
-/**
- * print_number - Prints an integer
- * @n: The integer to be printed
- */
-void print_number(int n)
-{
-if (n < 0) {
-_putchar('-');
-n = -n;
-}
-if (n / 10)
-print_number(n / 10);
-_putchar((n % 10) + '0');
-}
 int main(void)
 {
-int a[5] = {0, 1, 98, 3, 4};
-int *p;
+int a[5] = {0, 1, 2, 3, 4};
+int *p = &a[2];
 
-p = &a[2];
-printf("a[2] = %d\n", *p);
-print_number(98);
-_putchar('\n');
-print_number(402);
-_putchar('\n');
-print_number(1024);
-_putchar('\n');
-print_number(0);
-_putchar('\n');
-print_number(-98);
-_putchar('\n');
+p[0] = 98; 
+printf("a[2] = %d\n", a[2]);
 return (0);
 }
