fa394ac54b5b38b1336f19373ebed03f6c0d363d
siyasangabulana <bulanasiyasanga@gmail.com>
2023-07-03 04:26:05 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
  *_memset - it fiils a block of memory in hexa
  *@s:The address of memory to print
  *@n:number of bytes to be changed.
- *@b:desired value. 
+ *@b:desired value
  *Return: changed array with new value for n bytes
  */
 
