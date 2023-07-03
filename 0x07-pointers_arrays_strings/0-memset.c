25fccf802f372f7b248b126c2d8db499d1b4e3e8
siyasangabulana <bulanasiyasanga@gmail.com>
2023-07-03 04:06:41 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_memset - it fiils a block of memory in hexa
+ *@s:The address of memory to print
+ *@n:number of bytes to be changed.
+ *@b:desired value. 
+ *Return: changed array with new value for n bytes
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+s[i] = b;
+return (s);
+}
