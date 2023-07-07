2c010641859ef9949dc5a8096f06f367c0d98873
siyasangabulana <bulanasiyasanga@gmail.com>
2023-07-07 04:13:21 -0700

    0x09. C - Static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h" 
+
+/*
+ * _isupper - It check the upper letters
+ * @c: To check char
+ * Return: Uppercase 1 , else 0
+ */
+
+int _isupper(int c);
+{
+if (c => 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+return (0)
+}
---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*_memset - This program fills a block of memory with the specific value
+ * @b: Desired value
+ * @s: Address of memory filled
+ * @n: number of bytes changed
+ * Return: Array changes with new values for bytes
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+int a =0;
+for (; n > 0; a++)
+{
+s[a] = b;
+n--;
+}
+return (s);
+}
