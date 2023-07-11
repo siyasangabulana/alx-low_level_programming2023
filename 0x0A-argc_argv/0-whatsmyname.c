411f482232230a78dca4a2952a50a74e9d7b0077
siyasangabulana <bulanasiyasanga@gmail.com>
2023-07-11 02:43:12 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Prints name
+ *@argv: arguments vector
+ *@argc: arguments count
+ *Return: 0 Always
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
