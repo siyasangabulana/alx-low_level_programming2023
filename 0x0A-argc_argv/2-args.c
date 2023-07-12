0b71f261aae3091975865dd33d633a3da8472f47
siyasangabulana <bulanasiyasanga@gmail.com>
2023-07-12 05:44:31 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
- *main - Print each argument followed by a new line
- * Iterate over each argument using a loop
+ *main - Prints the arguments content
+ *@argc: arguments count
+ *@argv: arguments vector
  *Return: 0 Always
  */
 
-
 int main(int argc, char *argv[])
 {
-int i;
-for (int i = 0; i < argc; i++)
+for (int i = 1; i < argc; i++)
 {
 printf("%s\n", argv[i]);
 }
